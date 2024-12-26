960a40b5616a76785199b60d3af8b2fb6e43a76b
Youssouf da Silva <youssoufdasilva@gmail.com>
2024-12-26 20:07:51 +0000

    Update FeatureCardData with new project details and remove unused entry

--- src/components/FeatureCardData.tsx
+++ src/components/FeatureCardData.tsx
@@ -3,34 +3,31 @@ import type { CardDataProps } from "./FeatureSlideshow";
 export const cardData: CardDataProps[] = [
   {
     id: 1,
-    title: "Innovative Design",
-    content:
-      "Our design is not just visually appealing, but also highly functional.",
+    title: "Youssouf da Silva",
+    content: "My Main Website",
     image:
       "https://images.unsplash.com/photo-1720371300677-ba4838fa0678?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     id: 2,
-    title: "User-Centric Approach",
-    content:
-      "We prioritize the needs and preferences of our users in our design process.",
+    title: "Calavium",
+    content: "Calavium is one of my side projects",
     image:
       "https://images.unsplash.com/photo-1686170287433-c95faf6d3608?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8",
   },
   {
     id: 3,
-    title: "Seamless Integration",
-    content:
-      "Our features seamlessly integrate with your existing systems for a smooth experience.",
+    title: "Pixel Griot",
+    content: "Pixel Griot is a side project of mine",
     image:
       "https://images.unsplash.com/photo-1720378042271-60aff1e1c538?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxMHx8fGVufDB8fHx8fA%3D%3D",
   },
-  {
-    id: 4,
-    title: "Continuous Improvement",
-    content:
-      "We are constantly updating and improving our features to provide the best experience.",
-    image:
-      "https://images.unsplash.com/photo-1666882990322-e7f3b8df4f75?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDF8fHxlbnwwfHx8fHw%3D",
-  },
+  // {
+  //   id: 4,
+  //   title: "Continuous Improvement",
+  //   content:
+  //     "We are constantly updating and improving our features to provide the best experience.",
+  //   image:
+  //     "https://images.unsplash.com/photo-1666882990322-e7f3b8df4f75?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDF8fHxlbnwwfHx8fHw%3D",
+  // },
 ];
